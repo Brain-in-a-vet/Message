7a999ff96c73d77d4146238b842b1f11a0c098b5
zuoLand <chunyuan2008@163.com>
2018-01-26 13:43:58 +0800

    Update question

--- question/send_Question.sh
+++ question/send_Question.sh
@@ -4,12 +4,5 @@
 curDate=$(date "+%Y%m%d" -d "-1 days")
 
 #------------------------------------------------------------------------
-question=""
-
-while read line
-do
- question+="$line                             "
- question+="                   "
-done<questionList
-echo "$question" | mailx -v -s "问题_$curDate" chunyuan2008@163.com
+cat questionList | mailx -v -s "问题_$curDate" chunyuan2008@163.com
 
